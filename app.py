3f6651f77a0faa36b96671a8b34023d0e630fd3e
VeronicaLombardi89 <veronica.lombardi63@gmail.com>
2025-05-13 11:08:05 +0200

    la mia app

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
 import streamlit as st
+import io
+import pandas as pd
 
 def main():
-    st.title('la mia app')
+    st.title('La bottega del gusto')
+
+st.markdown(
+""""
+<style>
+body {
+    backround-color: #000000;
+    color: #FFFFFF;                           /* Imposta il colore del testo a bianco */ }
+}
+.stApp{ 
+    backround-color: #000000;
+}
+</style>
+"""
+
+
+st.title("La mia app con sfondo nero e testo bianco")
+st.write("Questo testo bianco è visualizzato su uno sfondo nero.")
+
+
 
 if __name__== "__main__":
     main()
