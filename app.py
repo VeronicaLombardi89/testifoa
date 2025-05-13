195c48f06220f3fa1a86447067b7b664b4fb26ce
VeronicaLombardi89 <veronica.lombardi63@gmail.com>
2025-05-13 11:41:39 +0200

    la mia app

--- app.py
+++ app.py
@@ -2,29 +2,17 @@ import streamlit as st
 import io
 import pandas as pd
 
-def main():
-    st.title('La bottega del gusto')
 
-# Inserire CSS corretto per lo stile della pagina
 st.markdown(
     """
     <style>
-    .stApp { 
-        background-color: #000000;
-        color: #FFFFFF;  /* Testo bianco */
-    }
-    h1, h2, h3, h4, h5, h6, p, div {
-        color: #FFFFFF !important;  /* Assicura che tutto il testo sia bianco */
-    }
+    .stApp { background-color: #000000; }
+    h1 { color: #FFFFFF; }
+    p { display: none; }  /* Nasconde qualsiasi testo normale */
     </style>
     """,
     unsafe_allow_html=True
 )
-
-# Titolo e contenuto della pagina
-st.title("La mia app con sfondo nero e testo bianco")
-st.write("Questo testo bianco è visualizzato su uno sfondo nero.")
+st.title("La bottega del gusto")
 
 
-if __name__== "__main__":
-    main()
